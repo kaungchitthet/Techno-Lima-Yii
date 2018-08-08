3f4722d696d355a0fde0851214d485f5bfd7f710
zarlinoo <zarzarlinoo806@gmail.com>
2018-08-08 13:59:49 +0630

    This is zar's commit

---
+++ Techno-Lima-Yii/Techno-Lima-Yii/models/Company.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "company".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $address
+ * @property string $type
+ * @property string $Email
+ */
+class Company extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'company';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['name', 'address', 'type', 'Email'], 'required'],
+            [['address'], 'string'],
+            ['Email', 'email'],
+            [['name', 'type', 'Email'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => 'Name',
+            'address' => 'Address',
+            'type' => 'Type',
+            'Email' => 'Email',
+        ];
+    }
+}
